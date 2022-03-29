9466e84f0a162d1d1afa53bb8eaffc77db4009ef
Bruce Tuhakaraina <Bruce.Tuhakaraina@toiohomai.ac.nz>
2022-03-30 11:11:16 +1300

    git is being a git

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+function Footer()
+
+{
+    return(
+
+        <div>
+            <div className="bg-secondary">
+                <p>Copyright Comp.6210 2022</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+function Header()
+{
+    return(
+
+        <div>
+            <nav className="navbar navbar-dark bg-dark">
+                <div className="navbar-brand">React Application Header</div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
